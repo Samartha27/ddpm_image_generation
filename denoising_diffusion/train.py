14a234cce2a2553a1d0a66c6fc7e26881138d8cf
Samartha Ramkumar <samartha.arks@gmail.com>
2022-12-15 14:37:37 -0800

    Updated

--- denoising_diffusion/train.py
+++ denoising_diffusion/train.py
@@ -9,7 +9,6 @@ from torchvision.utils import save_image
 from utils import constants, helpers
 import os
 from model.unet import Unet
-
 from dataloader import datasets
 from model.diffusion import Diffusion
 
@@ -20,46 +19,47 @@ if "PYTORCH_DEVICE" in os.environ:
 else:
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
+loader = datasets.get_loader(constants.DATA_FILEPATH, batch_size= constants.batch_size)
+
 model = Unet(
-
-    dim= constants.image_size,
-    channels= constants.channels,
-    dim_mults=(1, 2, 4, 8,)
-)
+    dim = constants.image_size,
+    channels = constants.channels,
+    dim_mults = constants.dim_mults
+    )
 model.to(device)
-
-diffusion = Diffusion(timesteps= constants.timesteps, device=device)
 
 optimizer = Adam(model.parameters(), lr=1e-3)
 
-loader = datasets.get_loader(constants.DATA_FILEPATH, batch_size= constants.batch_size)
+diffusion = Diffusion(timesteps= constants.timesteps, device=device)
 
 constants.RESULTS_DIR.mkdir(exist_ok = True)
 
-for epoch in range(constants.epochs):
-    for step, batch in enumerate(loader):
-        optimizer.zero_grad()
+if __name__ == '__main__':
 
-        batch_size = batch.shape[0]
-        batch = batch.to(device)
+    for epoch in range(constants.epochs):
+        for step, batch in enumerate(loader):
+            optimizer.zero_grad()
 
-        # Algorithm 1 line 3: sample t uniformally for every example in the batch
-        t = torch.randint(0, constants.timesteps, (batch_size,), device=device).long()
+            batch_size = batch.shape[0]
+            batch = batch.to(device)
 
-        loss = diffusion.p_losses(model, batch, t, loss_type="huber")
+            # Algorithm 1 line 3: sample t uniformally for every example in the batch
+            t = torch.randint(0, constants.timesteps, (batch_size,), device=device).long()
 
-        if step % 100 == 0:
-            print("Loss:", loss.item())
+            loss = diffusion.p_losses(model, batch, t, loss_type="huber")
 
-        loss.backward()
-        optimizer.step()
+            if step % 100 == 0:
+                print("Loss:", loss.item())
 
-        # save generated images
-        if step != 0 and step % constants.save_and_sample_every == 0:
-            milestone = step // constants.save_and_sample_every
-            batches = helpers.num_to_groups(4, batch_size)
-            all_images_list = list(map(lambda n: diffusion.sample(model, batch_size=n, channels=constants.channels), batches))
-            all_images = torch.cat(all_images_list, dim=0)
-            all_images = (all_images + 1) * 0.5
-            save_image(all_images, str(constants.RESULTS_DIR / f'sample-{milestone}.png'), nrow = 6)
+            loss.backward()
+            optimizer.step()
+
+            # save generated images
+            if step != 0 and step % constants.save_and_sample_every == 0:
+                milestone = step // constants.save_and_sample_every
+                batches = helpers.num_to_groups(4, batch_size)
+                all_images_list = list(map(lambda n: diffusion.sample(model, batch_size=n, channels=constants.channels), batches))
+                all_images = torch.cat(all_images_list, dim=0)
+                all_images = (all_images + 1) * 0.5
+                save_image(all_images, str(constants.RESULTS_DIR / f'sample-{milestone}.png'), nrow = 6)
 
--- denoising_diffusion/utils/constants.py
+++ denoising_diffusion/utils/constants.py
@@ -6,3 +6,7 @@ image_size = 64
 channels = 3
 epochs = 5
 save_and_sample_every = 1000
+dim_mults = (1, 2, 4, 8,)
+
+
+
