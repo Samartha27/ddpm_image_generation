76a5a3e5da1d923ff00fd63c500ef9e59ea83bf9
Samartha Ramkumar <samartha.arks@gmail.com>
2022-12-15 11:49:03 -0800

    Updated

--- denoising_diffusion/train.py
+++ denoising_diffusion/train.py
@@ -1,19 +1,15 @@
 import numpy as np
-import copy
 from pathlib import Path
 from random import random
 from collections import namedtuple
-from multiprocessing import cpu_count
 import torch
-from torch import nn, einsum
-import torch.nn.functional as F
 from tqdm.auto import tqdm
 from torch.optim import Adam
 from torchvision.utils import save_image
-from einops import rearrange, reduce, repeat
-from einops.layers.torch import Rearrange
-from utils import constants
+from utils import constants, helpers
 from dataloader import dataloader
+from model.unet import Unet
+from model import forward_diffusion
 
 
 results_folder = Path("./results")
@@ -46,7 +42,7 @@ for epoch in range(constants.epochs):
       # Algorithm 1 line 3: sample t uniformally for every example in the batch
       t = torch.randint(0, constants.timesteps, (batch_size,), device=device).long()
 
-      loss = p_losses(model, batch, t, loss_type="huber")
+      loss = forward_diffusion.p_losses(model, batch, t, loss_type="huber")
 
       if step % 100 == 0:
         print("Loss:", loss.item())
@@ -57,7 +53,7 @@ for epoch in range(constants.epochs):
       # save generated images
       if step != 0 and step % save_and_sample_every == 0:
         milestone = step // save_and_sample_every
-        batches = num_to_groups(4, batch_size)
+        batches = helpers.num_to_groups(4, batch_size)
         all_images_list = list(map(lambda n: sample(model, batch_size=n, channels= constants.channels), batches))
         all_images = torch.cat(all_images_list, dim=0)
         all_images = (all_images + 1) * 0.5
